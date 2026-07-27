e637f52f9001b5e15b4d418966792cb23bd12b49
blueladd11-commits-tocode <blueladd11@icloud.com>
2026-07-27 11:40:40 +0500

    Add Firebase project configuration

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCefTox6j07VHzQQhO9T7KXLcLA2K9JzY8",
+  authDomain: "lvfc-player-tracker.firebaseapp.com",
+  projectId: "lvfc-player-tracker",
+  storageBucket: "lvfc-player-tracker.firebasestorage.app",
+  messagingSenderId: "699991327989",
+  appId: "1:699991327989:web:4d6612b9470644013fb1bc",
+  measurementId: "G-3TSX6DYL33"
+};
