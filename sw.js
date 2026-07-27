aea8ed5ca3afa5d1c10748c3f6d3433973c7c74f
blueladd11-commits-tocode <blueladd11@icloud.com>
2026-07-27 14:29:20 +0500

    Reset cache for LVFC KIXEL deployment

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='lvfc-v6';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./lvfc-logo-white.svg'];
+const CACHE='lvfc-kixel-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./lvfc-logo-white.svg','./lvfc-kixel-icon.svg','./kixel-logo.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
